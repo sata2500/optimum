0530a38bc35022c0126f5b83ec357df0600e52d9
Salih Evin <salih@optimum.tech>
2026-08-13 14:44:43 +0300

    bump: Update versionCode to 33 and versionName to 1.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "tech.salev.optimum"
         minSdk = 26
         targetSdk = 36
-        versionCode = 32
-        versionName = "1.5.0"
+        versionCode = 33
+        versionName = "1.6.0"
     }
 
     signingConfigs {
